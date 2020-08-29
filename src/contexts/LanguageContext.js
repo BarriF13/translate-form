93f5075b57ef1bd6d5987991456096e605bc3faa
Barri <info@bdesignstudio.co.uk>
2020-08-29 12:09:52 +0100

    add language store

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,4 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 //set up the default language to english
-export default React.createContext( 'english');
+const Context = React.createContext('english');
+
+
+export class LanguageStore extends Component {
+  //------------business logic 
+  //can be used by other component
+  state = { language: 'english' };
+  //can change language by other component
+  onLanguageChange = (language) => {
+    this.setState({ language })
+
+  }
+
+  render(){
+    return(
+      <Context.Provider value={ {...this.state, onLanguageChange } }>
+        {this.props.children}
+      </Context.Provider>
+    )
+  }
+}
+export default Context;
