e0d6daed70cafff1f09bac352279d202b89c98be
Barri <info@bdesignstudio.co.uk>
2020-08-29 11:47:47 +0100

    separate a language component

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,20 @@ import UserCreate from '../components/UserCreate';
 //we are making provider so we need the language context
 import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from '../components/LanguageSelector';
 
 export class App extends Component {
   state = { language: 'english' };
 
+ 
   onLanguageChange = language => {
     this.setState({ language })
   }
 
-
   render() {
     return (
       <div className="ui container">
-        <div>
-          select a language:
-        <i className="flag uk" onClick={() => this.onLanguageChange('english')}></i>
-          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')}></i>
-        </div>
+       <LanguageSelector onLanguageChange={this.onLanguageChange}/>
         {/* UserCreate needs to get some info form context obj because button and field are in there so we wrap it with provider and context then give value  */}
         <LanguageContext.Provider value={this.state.language}>
           <ColorContext.Provider value='teal'>
---
+++ src/components/LanguageSelector.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class LanguageSelector extends Component {
+ 
+  render() {
+    return (
+      <div>
+        select a language:
+        <i className="flag uk" onClick={() => this.props.onLanguageChange('english')}></i>
+        <i className="flag nl" onClick={() => this.props.onLanguageChange('dutch')}></i>
+      </div>
+    )
+  }
+}
+
+export default LanguageSelector
