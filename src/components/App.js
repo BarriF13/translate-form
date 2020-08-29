fd5422bfaf3c806e78bcdefd0e6c16d2a512cdb2
Barri <info@bdesignstudio.co.uk>
2020-08-29 12:18:25 +0100

    render language store

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react'
 import UserCreate from '../components/UserCreate';
+import { LanguageStore } from '../contexts/LanguageContext'
 //we are making provider so we need the language context
 import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 import LanguageSelector from '../components/LanguageSelector';
 
 export class App extends Component {
-  state = { language: 'english' };
+  // state = { language: 'english' };
 
- 
-  onLanguageChange = language => {
-    this.setState({ language })
-  }
+
+  // onLanguageChange = language => {
+  //   this.setState({ language })
+  // }
 
   render() {
     return (
       <div className="ui container">
-       <LanguageSelector onLanguageChange={this.onLanguageChange}/>
-        {/* UserCreate needs to get some info form context obj because button and field are in there so we wrap it with provider and context then give value  */}
-        <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          {/* <LanguageSelector onLanguageChange={this.onLanguageChange} /> */}
+          <LanguageSelector/>
+          {/* UserCreate needs to get some info form context obj because button and field are in there so we wrap it with provider and context then give value  */}
+          {/* <LanguageContext.Provider value={this.state.language}> */}
           <ColorContext.Provider value='teal'>
             <UserCreate />
           </ColorContext.Provider>
-        </LanguageContext.Provider>
+        </LanguageStore>
+        {/* </LanguageContext.Provider> */}
         {/* {this.state.language} */}
       </div>
     )
